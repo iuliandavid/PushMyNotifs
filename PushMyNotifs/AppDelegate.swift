04114bc6ed0752eed2f2f7668d92aa83fcb33008
Iulian David <david.iulian@gmail.com>
2017-08-16 06:51:18 +0300

    small fixes

--- PushMyNotifs/AppDelegate.swift
+++ PushMyNotifs/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,32 +16,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+       
+        if #available(iOS 8.0, *) {
+            let settings :UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+            application.registerForRemoteNotifications()
+        } else {
+            let types: UIRemoteNotificationType = [.alert, .badge, .sound]
+            application.registerForRemoteNotifications(matching: types)
+        }
+        
+        //add Firebase config
+        FIRApp.configure()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.tokenRefreshNotification(notification:)), name: NSNotification.Name.firInstanceIDTokenRefresh, object: nil)
         return true
+        
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        //clean up code
+        FIRMessaging.messaging().disconnect()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        connecToFcm()
     }
 
+   
 
+    func tokenRefreshNotification(notification: NSNotification) throws {
+        guard let refreshedToken = FIRInstanceID.instanceID().token() else {
+            fatalError()
+        }
+        print("InstanceID token: \(refreshedToken)")
+        connecToFcm()
+        
+    }
+    
+    /**
+     In order to work with Firebase we need to connect to
+     FirebaseCentralMessaging
+    */
+    func connecToFcm(){
+        FIRMessaging.messaging().connect(completion: {
+            error in
+            if (error != nil) {
+                print("Unable to connect to Firebase \(error.debugDescription)")
+            } else {
+                print("connected to FCM")
+            }
+        })
+    }
+    
+    
 }
 
